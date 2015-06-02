459d8d6652e8365045bc6c3634ae64422afee0c0
Ondrej Kousal <ok@sunnyrentals.com>
2015-06-02 15:52:51 +0200

    Removed wrong test

---
+++ src/Vacasol/Catalog/Value/BookingResult.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Vacasol\Catalog\Value;
+
+use Vacasol\Catalog\Value;
+
+class BookingResult extends Value {
+    /**
+     * @var string $OrderNumber
+     */
+    protected $OrderNumber;
+
+    /**
+     * @var string $BookingId
+     */
+    protected $BookingId;
+}
